f1a8057e2b781a7d790c81d64517a9bf58155071
Mackrina10 <heavenmhally@gmail.com>
2025-05-06 23:23:31 +0300

    Hide each slide

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ const totalSlides = slides.length;
 
 function showSlide(index) {
     // Loop through all slides and hide them
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
 }
