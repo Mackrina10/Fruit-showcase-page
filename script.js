e584b282b3f6208473691d2582843f187a50b130
Mackrina10 <heavenmhally@gmail.com>
2025-05-06 23:22:34 +0300

    Loop through all slides and hide them

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // JavaScript to handle the slideshow functionality
+
 let currentSlide = 0; 
+
 const slides = document.querySelectorAll('.slide-item'); 
 const totalSlides = slides.length; 
+
+function showSlide(index) {
+    // Loop through all slides and hide them
+}
